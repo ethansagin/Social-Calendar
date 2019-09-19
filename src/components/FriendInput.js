43d97f1c308833bbb0cc085bd825f8f12753f3bb
ethansagin <ethansagin@gmail.com>
2019-09-19 09:38:13 -0400

    implement button for add friend form

--- src/components/FriendInput.js
+++ src/components/FriendInput.js
@@ -10,8 +10,16 @@ class FriendInput extends Component {
             visit_interval: 1,
             last_meetup: '',
             next_meetup: '',
-            loading: false
+            loading: false,
+            showForm: false
         }
+    }
+
+    handleClick = (e) => {
+        e.preventDefault()
+        this.setState({
+            showForm: true
+        })
     }
 
     handleChange = (e) => {
@@ -28,28 +36,35 @@ class FriendInput extends Component {
             name: '',
             visit_interval: 1,
             last_meetup: '',
-            loading: false
+            loading: false,
+            showForm: false
         })
     }
 
     render() {
+        const form =
+        <form onSubmit={this.handleSubmit}>
+            <label>Your Friend's Name</label>
+            <input type='text' name='name' value={this.state.name} onChange={this.handleChange} /><br/>
+
+            <label>How Often (in Months) Would You Like to Meetup?</label>
+            <input type='number' name='visit_interval' value={this.state.visit_interval} onChange={this.handleChange} /><br/>
+
+            <label>When Was the Last Time You Got Together?</label>
+            <input type='date' name='last_meetup' value={this.state.last_meetup} onChange={this.handleChange} /><br/>
+
+            <input type='submit' value='Add Friend' /><br/>
+
+            <hr/>
+
+        </form>
+
+    const button =
+        <button onClick={this.handleClick}>Add a Friend!</button>
+
         return(
             <div className='friend-input'>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Your Friend's Name</label>
-                    <input type='text' name='name' value={this.state.name} onChange={this.handleChange} /><br/>
-
-                    <label>How Often (in Months) Would You Like to Meetup?</label>
-                    <input type='number' name='visit_interval' value={this.state.visit_interval} onChange={this.handleChange} /><br/>
-
-                    <label>When Was the Last Time You Got Together?</label>
-                    <input type='date' name='last_meetup' value={this.state.last_meetup} onChange={this.handleChange} /><br/>
-
-                    <input type='submit' value='Add Friend' /><br/>
-
-                    <hr/>
-
-                </form>
+                {this.state.showForm ? form : button}
             </div>
         )
     }

95fe4d8fd81940e60356be3ba6ce5fbfe2f1ef29
ethansagin <ethansagin@gmail.com>
2019-09-19 16:14:52 -0400

    change FriendList component to presentational format

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,27 +1,18 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class FriendsList extends Component {
-    render() {
-        const friendLink = (friend) => 
-            <Link to={`friends/${friend.id}`}>
-                <button>{friend.name}</button>
-            </Link>
-        
+const friendLink = (friend) => 
+    <Link to={`friends/${friend.id}`}>
+        <button>{friend.name}</button>
+    </Link>
 
-        const friendsList = this.props.friends.map((f, i) => 
-                <li key={i}>
-                    {friendLink(f)} <br/>
-                    Next Meetup: {f.next_meetup}
-                </li>
-        )
 
-        return(
-            <ul>
-                {friendsList}
-            </ul>
-        )
-    }
-}
+const FriendsList = (props) => props.friends.map((f, i) => 
+    <li key={i}>
+        {friendLink(f)} <br/>
+        Next Meetup: {f.next_meetup}
+    </li>
+)
+
 
 export default FriendsList

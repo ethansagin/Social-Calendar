22fe95af513e667cddbc57fd1d73007a18f84833
ethansagin <ethansagin@gmail.com>
2019-09-24 16:23:37 -0400

    add basic text to homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return(
-            <h2>Welcome to the React Social Calendar!</h2>
+            <div className='home'>
+                <h2>Welcome to the React Social Calendar!</h2>
+                <p>
+                    This is Ethan Sagin's final project for the Flatiron Online Software Engineering Bootcamp.<br/>
+                    Keep track of your friends to make sure you're meeting with them on a regular basis!<br/>
+                    
+                    <strong>Note: </strong>This app uses a Rails API to store your information. Make sure that you have started the API in a second terminal before using (see README for further instructions)
+
+                </p>
+            </div>
         )
     }
 }

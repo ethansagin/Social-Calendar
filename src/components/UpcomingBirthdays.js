5dd2d3d121ae7a7d120529e22b42ab131597c8c5
ethansagin <ethansagin@gmail.com>
2019-09-24 13:29:36 -0400

    sort birthdays by month in UpcomingBirthdays container

--- src/components/UpcomingBirthdays.js
+++ src/components/UpcomingBirthdays.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { formatBirthDate } from '../helpers'
 
 const sortedBirthdayLis = (arr) => {
-    return arr.sort((a, b) => (a.birthday > b.birthday) ? 1 : -1).map((f, i) =>
-        <li key={i}>
-            {formatBirthDate(f.birthday)} - {f.name}
-        </li>    
-    )
+    return arr.sort((a, b) => 
+        ((new Date(a.birthday).getMonth()) > (new Date(b.birthday).getMonth())) ? 1 : -1)
+        .map((f, i) =>
+            <li key={i}>
+                {formatBirthDate(f.birthday)} - {f.name}
+            </li>    
+        )
 }
 
 const UpcomingBirthdays = (props) => 

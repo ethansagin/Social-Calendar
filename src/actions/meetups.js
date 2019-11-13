dcdd88d0f89d96906eab8086d96c0318f77a435d
ethansagin <ethansagin@gmail.com>
2019-11-13 14:26:17 -0500

    remove http:// before origin in cors

--- src/actions/meetups.js
+++ src/actions/meetups.js
@@ -1,4 +1,4 @@
-const API_URL = "https://social-calendar-api-postgresql.herokuapp.com/"
+const API_URL = 'https://social-calendar-api-postgresql.herokuapp.com/'
 
 export const getMeetups = () => {
     return (dispatch) => {
